5514da85ac81fed8ce1c8de9867516a54506b382
KathrynOliveira <k.olliver18@gmail.com>
2026-01-08 15:37:57 -0300

    class adjustment

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -7,8 +7,9 @@ function TaskPage() {
   const [searchParams] = useSearchParams();
   const title = searchParams.get("title");
   const description = searchParams.get("description");
+
   return (
-    <div className="w-screen h-screen bg-slate-500 p-6">
+    <div className="w-screen h-screen bg-slate-500 p-6 flex justify-center">
       <div className="w-[500px] space-y-4">
         <div className="flex justify-center relative mb-6">
           <button
@@ -19,7 +20,7 @@ function TaskPage() {
           </button>
         </div>
         <Title>Detalhes de Tarefa</Title>
-        <div className="bg-slate-200 p-4 rounded-md-">
+        <div className="bg-slate-200 p-4 rounded-md">
           <h2 className="text-xl font-bold text-slate-600">{title}</h2>
           <p className=" text-slate-600">{description}</p>
         </div>
