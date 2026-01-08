3314a86031b8e08b73518df918d7a6421872f898
KathrynOliveira <k.olliver18@gmail.com>
2026-01-08 15:29:39 -0300

    compounding

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Task";
 import { v4 } from "uuid";
+import Title from "./components/Title";
 
 function App() {
   const [tasks, setTasks] = useState(
@@ -12,6 +13,26 @@ function App() {
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
+
+  // useEffect(() => {
+  //   async function fecthTasks() {
+  //     // só consegue usar async/await no useEffect se for uma function
+  //     //CHAMAR API
+  //     const response = await fetch(
+  //       "https://jsonplaceholder.typicode.com/todos?_limit=10",
+  //       {
+  //         method: "GET",
+  //       }
+  //     );
+  //     //PEGAR OS DADOS QUE ELA RETORNA
+  //     const data = await response.json();
+
+  //     //ARMAZENA/PRESISTIR ESSES DADOS NO STATE
+  //     setTasks(data);
+  //   }
+
+  //   fecthTasks();
+  // }, []);
 
   function onTaskClick(taskId) {
     const newTasks = tasks.map((task) => {
@@ -41,9 +62,7 @@ function App() {
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px] space-y-4">
-        <h1 className="text-3xl text-slate-100 font-bold text-center">
-          Gerenciador de Tarefas
-        </h1>
+        <Title> Gerenciador de Tarefas</Title>
         <AddTask onAddTaskSubmit={onAddTaskSubmit} />
         <Tasks
           tasks={tasks}
